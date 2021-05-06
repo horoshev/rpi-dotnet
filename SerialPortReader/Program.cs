be1e95e33a99d5a372214a51f5e09cd96673355b
Khoroshev Artem <horoshev.artem@yandex.ru>
2021-05-06 21:12:58 +0700

    Read from serial port.

---
+++ SerialPortReader/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace SerialPortReader
+{
+    class Program
+    {
+        private static readonly SerialPort WeightSerialPort = new("/dev/serial0");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (WeightSerialPort.IsOpen)
+            {
+                var line = WeightSerialPort.ReadLine();
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
